a9030b02f16aa62846af3c0fc3f7e32fbb845657
Brad Redpath <bre58@uclive.ac.nz>
2023-10-10 18:13:00 +1300

    wheel mount

---
+++ Code/lib/LFR/src/LFR.h
@@ -0,0 +1,16 @@
+#ifndef LFR_H
+#define LFR_H
+
+#define L_MOTOR PIN_PA1
+#define R_MOTOR PIN_PA2
+#define BUZZER_PIN PIN_PA3
+#define LED_1 PIN_PF2
+#define LED_2 PIN_PF3 
+#define LED_3 PIN_PF4
+#define IR_1 PIN_PD0 
+#define IR_2 PIN_PD1 
+#define IR_3 PIN_PD2 
+#define IR_4 PIN_PD3 
+#define IR_5 PIN_PD4 
+
+#endif //LFR_H
--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
-
-#define LED_PIN PIN_PF2
-#define BUZZER_PIN PIN_PA3
+#include "LFR.h"
+#define BUZZER PIN_PA3
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_1, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(R_MOTOR, OUTPUT);
+  pinMode(L_MOTOR, OUTPUT);
+
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for (int speed = 0; speed <= 255; speed += 5) { // Change 5 to adjust step size
+    analogWrite(L_MOTOR, speed);
+    analogWrite(R_MOTOR, speed);
+    delay(50); // Adjust delay for acceleration rate
+  }
+
+  delay(1000); // Run at full speed for 1 second
+
+  // Decelerate motors
+  for (int speed = 255; speed >= 0; speed -= 5) { // Change 5 to adjust step size
+    analogWrite(L_MOTOR, speed);
+    analogWrite(R_MOTOR, speed);
+    delay(50); // Adjust delay for deceleration rate
+  }
+
+  delay(1000); // Pause for 1 second
   tone(BUZZER_PIN, 1000);
   delay(1000);
   noTone(BUZZER_PIN);
   delay(1000);
+  digitalWrite(LED_1,HIGH); //Turns on LED
+  delay(1000);
+  digitalWrite(LED_1, LOW); //Turn off LED
+  delay(1000);
+
 }
+
