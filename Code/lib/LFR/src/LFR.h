2f09c48c25e1d540f74fb2661cc582f76627c259
r3dpath <137386245+r3dpath@users.noreply.github.com>
2023-10-12 11:29:38 +1300

    Coding stuff

--- Code/lib/LFR/src/LFR.h
+++ Code/lib/LFR/src/LFR.h
@@ -7,10 +7,19 @@
 #define LED_1 PIN_PF2
 #define LED_2 PIN_PF3 
 #define LED_3 PIN_PF4
+
 #define IR_1 PIN_PD0 
 #define IR_2 PIN_PD1 
 #define IR_3 PIN_PD2 
 #define IR_4 PIN_PD3 
 #define IR_5 PIN_PD4 
+
+typedef enum {
+    LEFTMOTOR,
+    RIGHTMOTOR
+} motor_t;
+
+void sensorRead(uint8_t *sensors);
+void setMotor(uint8_t speedL, uint8_t speedR);
 
 #endif //LFR_H
--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,43 +1,41 @@
 #include <Arduino.h>
 #include "LFR.h"
-#define BUZZER PIN_PA3
+
+#define WRITEFREQ 1 //Supports 1, 2, 8, 16, 32 and 64 kHz
+#define ANALOGRES 8 //Supports 8 and 10 bit resolution
+
+uint8_t sensors[5] = {0, 0, 0, 0, 0};
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(R_MOTOR, OUTPUT);
   pinMode(L_MOTOR, OUTPUT);
+  pinMode(IR_1, INPUT);
+  pinMode(IR_2, INPUT);
+  pinMode(IR_3, INPUT);
+  pinMode(IR_4, INPUT);
+  pinMode(IR_5, INPUT);
 
-  
+  Serial1.begin(9600);
+  analogWriteFrequency(WRITEFREQ);
+  analogReadResolution(ANALOGRES);
+}
+
+void printSensors() {
+  sensorRead(sensors);
+  for (int i = 0; i < 5; i++) {
+    Serial1.print(sensors[i]);
+    Serial1.print(" ");
+  }
+  Serial1.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for (int speed = 0; speed <= 255; speed += 5) { // Change 5 to adjust step size
-    analogWrite(L_MOTOR, speed);
-    analogWrite(R_MOTOR, speed);
-    delay(50); // Adjust delay for acceleration rate
-  }
-
-  delay(1000); // Run at full speed for 1 second
-
-  // Decelerate motors
-  for (int speed = 255; speed >= 0; speed -= 5) { // Change 5 to adjust step size
-    analogWrite(L_MOTOR, speed);
-    analogWrite(R_MOTOR, speed);
-    delay(50); // Adjust delay for deceleration rate
-  }
-
-  delay(1000); // Pause for 1 second
-  tone(BUZZER_PIN, 1000);
-  delay(1000);
-  noTone(BUZZER_PIN);
-  delay(1000);
-  digitalWrite(LED_1,HIGH); //Turns on LED
-  delay(1000);
-  digitalWrite(LED_1, LOW); //Turn off LED
-  delay(1000);
-
+  printSensors();
+  delay(100);
 }
 
